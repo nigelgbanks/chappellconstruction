0cc0e2ccb59704ee9c8115d355441c185ea4bbad
Nigel Banks <nigel.g.banks@gmail.com>
2014-02-16 16:35:29 -0400

    Sample assets.

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file calls the init.php file, but only
+ * if the child theme hasn't called it first.
+ *
+ * This method allows the child theme to load
+ * the framework so it can use the framework
+ * components immediately.
+ *
+ * This file is a core Genesis file and should not be edited.
+ *
+ * @category Genesis
+ * @package  Templates
+ * @author   StudioPress
+ * @license  GPL-2.0+
+ * @link     http://my.studiopress.com/themes/genesis
+ */
---
+++ home.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Chappell Construction Home Page Theme.
+ *
+ * @package Genesis\Templates
+ * @subpackage Chappell Construction
+ * @author  Nigel Banks
+ * @license GPL-2.0+
+ * @link    http://nigelbanks.ca
+ */
