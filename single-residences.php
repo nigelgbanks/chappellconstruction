f5aef68c4257049a1ec2c112ed91e39c18577635
Nigel Banks <nigel.g.banks@gmail.com>
2014-03-02 16:13:57 -0400

    Residence comming along

--- single-residences.php
+++ single-residences.php
@@ -9,5 +9,9 @@
 // Force sidebar-content layout.
 add_filter('genesis_pre_get_option_site_layout', '__genesis_return_sidebar_content');
 
+// Only Display the content.
+remove_all_actions('genesis_entry_header');
+remove_all_actions('genesis_entry_footer');
+
 genesis();
 ?>
